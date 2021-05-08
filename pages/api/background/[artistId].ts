d22bc63cc1bc826ae82deaa42593e445c38bba0c
mieszko <mieszko.mac@gmail.com>
2021-05-08 23:52:06 +0200

    Revert "puppeteer pool"
    This reverts commit 7e21a683462f63687a7a53d9427efa1ead154807.

--- pages/api/background/[artistId].ts
+++ pages/api/background/[artistId].ts
@@ -1,20 +1,12 @@
 import nc from 'next-connect';
 import { NextApiResponse, NextApiRequest } from "next";
-import genericPool from 'generic-pool';
-import puppeteer, { Browser } from 'puppeteer';
+import puppeteer from 'puppeteer';
 import { promisify } from "util";
 import redis from 'redis';
-
 const cache = redis.createClient();
+
 const getAsync = promisify(cache.get).bind(cache);
 const setAsync = promisify(cache.set).bind(cache);
-
-const factory = {
-  create: () => puppeteer.launch(),
-  destroy: (browser: Browser) => browser.close()
-};
-
-const puppeteerPool = genericPool.createPool(factory, { max: 7, min: 2});
 
 const handler = nc<NextApiRequest, NextApiResponse>();
 
@@ -26,30 +18,27 @@ handler.get(async (req, res) => {
   if (redisResult != null) {
     return res.json(redisResult);
   }
+
+  const browser = await puppeteer.launch();
   try {
-    const browser = await puppeteerPool.acquire();
-    try {
-      const page = await browser.newPage();
-      await page.setViewport({
-        width: 1920,
-        height: 1080
-      });
-      await page.goto(`${SPOTIFY_ARTIST_URL}${artistId}`);
-      await page.waitForSelector('[data-testid=background-image]', { timeout: 10_000 });
-      const bgUrl = await page.evaluate(() => {
-        const res = (document.querySelector('[data-testid=background-image]') as any)?.style?.backgroundImage;
-        return res;
-      });
-      const retrievedUrl = bgUrl ? JSON.stringify(bgUrl.match(/"([^"]+)"/)[1]) : null;
-      await setAsync(artistId, retrievedUrl);
-      res.json(retrievedUrl);
-    } catch (error) {
-      res.json(null);
-    } finally {
-      await puppeteerPool.release(browser);
-    }
-  } catch (_) {
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1920,
+      height: 1080
+    });
+    await page.goto(`${SPOTIFY_ARTIST_URL}${artistId}`);
+    await page.waitForSelector('[data-testid=background-image]', { timeout: 10_000 });
+    const bgUrl = await page.evaluate(() => {
+      const res = (document.querySelector('[data-testid=background-image]') as any)?.style?.backgroundImage;
+      return res;
+    });
+    const retrievedUrl = bgUrl ? JSON.stringify(bgUrl.match(/"([^"]+)"/)[1]) : null;
+    await setAsync(artistId, retrievedUrl);
+    res.json(retrievedUrl);
+  } catch (error) {
     res.json(null);
+  } finally {
+    await browser.close();
   }
 });
 
