440aa7f17615f7e57362f52d4b9b2662de5bcd51
Mieszko Sabo <mieszko.mac@gmail.com>
2022-01-04 10:30:55 +0100

    rm background images feature

--- hooks/useSpotifyData.ts
+++ hooks/useSpotifyData.ts
@@ -19,22 +19,23 @@ const parseData = (data: any): PlayerData => ({
   duration: data?.data?.item?.duration_ms,
   progress: data?.data?.progress_ms,
   paused: data?.data?.is_playing ? false : true,
-  albumSrc: data?.data?.item?.album?.images[0]?.url
+  albumSrc: data?.data?.item?.album?.images[0]?.url,
 });
 
-export const useSpotifyData = (accessToken: string | null, options: Options = { refreshInterval: 5_000 }) => {
+export const useSpotifyData = (
+  accessToken: string | null,
+  options: Options = { refreshInterval: 5_000 }
+) => {
   const { data, error } = useSWR(
     accessToken ? [SPOTIFY_API_URL, accessToken] : null,
     fetchWithToken,
     options
   );
-  const artistId = data?.data?.item?.artists[0]?.id;
-  const { data: artistImg } = useSWR(() => artistId ? `/api/background/${artistId}` : null);
   const isLoading = data === undefined;
   return {
     isError: error,
     isNotPlaying: !isLoading && (data.data == null || data.data === ""),
     data: parseData(data),
-    artistImg: artistImg?.bgUrl
+    artistImg: null,
   };
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,99 +1,127 @@
-import querystring from 'query-string';
+import querystring from "query-string";
 import { useFullScreenHandle } from "react-full-screen";
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import { useMachine } from '@xstate/react';
-import { machine } from '../machines';
-import { useSpotifyData } from '../hooks/useSpotifyData';
-import { PlayerLayout } from '../components/PlayerLayout';
-import { Main } from '../components/layout/Main';
-import { usePalette } from '../hooks/usePalette/usePalette';
-import { IconButton } from '../components/IconButton';
-import { Topbar } from '../components/layout/Topbar';
-import { enableNoSleep } from '../utils/nosleep';
-import { FullscreenIcon } from '../components/Icons/FullscreenIcon';
-import { SwitchDisplay } from '../components/Icons/SwitchIcon';
-import { Palette } from '../components/Icons/PaletteIcon';
-import { Flex } from '../components/layout/Flex';
-import { Text } from '../components/layout/Text';
-import useSWR from 'swr';
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useMachine } from "@xstate/react";
+import { machine } from "../machines";
+import { useSpotifyData } from "../hooks/useSpotifyData";
+import { PlayerLayout } from "../components/PlayerLayout";
+import { Main } from "../components/layout/Main";
+import { usePalette } from "../hooks/usePalette/usePalette";
+import { IconButton } from "../components/IconButton";
+import { Topbar } from "../components/layout/Topbar";
+import { enableNoSleep } from "../utils/nosleep";
+import { FullscreenIcon } from "../components/Icons/FullscreenIcon";
+import { Palette } from "../components/Icons/PaletteIcon";
+import { Flex } from "../components/layout/Flex";
+import { Text } from "../components/layout/Text";
+import useSWR from "swr";
 
 export default function Home(): JSX.Element {
   const router = useRouter();
   const fullscreen = useFullScreenHandle();
   const [state, send] = useMachine(machine);
   const { context } = state;
-  const { data, 
-          isError,
-          isNotPlaying,
-          artistImg
-  } = useSpotifyData(context.accessToken);
-  const { data: tokens } = useSWR(context.refreshToken ? `/api/refresh_token/${context.refreshToken}` : null, { refreshInterval: 1000 * 60 * 30}); // refresh token every 30 mins
+  const { data, isError, isNotPlaying, artistImg } = useSpotifyData(
+    context.accessToken
+  );
+  const { data: tokens } = useSWR(
+    context.refreshToken ? `/api/refresh_token/${context.refreshToken}` : null,
+    { refreshInterval: 1000 * 60 * 30 }
+  ); // refresh token every 30 mins
   useEffect(() => {
     if (!tokens) {
       return;
     }
-    send({ type: 'TOKEN_UPDATE', accessToken: tokens.access_token, refreshToken: tokens.refresh_token});
+    send({
+      type: "TOKEN_UPDATE",
+      accessToken: tokens.access_token,
+      refreshToken: tokens.refresh_token,
+    });
   }, [tokens]);
   const { data: palette } = usePalette(data.albumSrc);
   useEffect(() => {
-    if (state.value !== 'noAuth') {
+    if (state.value !== "noAuth") {
       return;
     }
     const { query } = querystring.parseUrl(window.location.href);
     const { accessToken, refreshToken } = query;
     if (accessToken == null || refreshToken == null) {
-      router.push('/api/login');
-    }
-    else {
+      router.push("/api/login");
+    } else {
       send({
-        type: 'TOKEN_UPDATE', 
+        type: "TOKEN_UPDATE",
         accessToken: accessToken as string,
-        refreshToken: refreshToken as string
+        refreshToken: refreshToken as string,
       });
     }
   }, [state.value]);
 
   if (isError) {
-    send({ type: 'AUTH_ERROR'});
+    send({ type: "AUTH_ERROR" });
   }
-  
+
   if (isNotPlaying) {
-    send({ type: 'NO_DATA'});
+    send({ type: "NO_DATA" });
+  } else {
+    send({ type: "DATA_RECEIVED" });
   }
-  else {
-    send({ type: 'DATA_RECEIVED'});
-  }
-  if (state.value === 'loading' || state.value === 'noAuth' || state.value === 'notPlaying') {
+  if (
+    state.value === "loading" ||
+    state.value === "noAuth" ||
+    state.value === "notPlaying"
+  ) {
     return (
-    <Main fullScreenHandle={fullscreen}>
-      <Flex justifyContent="center" alignItems="center" width="full" height="full">
-        <Text fontSize="3rem" fontWeight="bold" color="white">
-          {state.value === 'notPlaying' ? "Play something" : "Loading..."}
-        </Text>
-      </Flex>
-    </Main>
+      <Main fullScreenHandle={fullscreen}>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          width="full"
+          height="full"
+        >
+          <Text fontSize="3rem" fontWeight="bold" color="white">
+            {state.value === "notPlaying" ? "Play something" : "Loading..."}
+          </Text>
+        </Flex>
+      </Main>
     );
   }
   return (
-    <Main 
-      backgroundColor={context.paletteMode === 'VIBRANT' ? palette.darkVibrant : palette.darkMuted}
+    <Main
+      backgroundColor={
+        context.paletteMode === "VIBRANT"
+          ? palette.darkVibrant
+          : palette.darkMuted
+      }
       backgroundImg={artistImg}
       displayMode={context.displayMode}
       fullScreenHandle={fullscreen}
     >
-      {!fullscreen.active && <Topbar>
-        <IconButton onClick={() => send({ type: 'SWITCH_DISPLAY'})} icon={SwitchDisplay} />
-        <IconButton onClick={() => send({ type: 'SWITCH_PALETTE'})} icon={Palette} />
-        <IconButton onClick={() => { fullscreen.enter(); enableNoSleep(); }} icon={FullscreenIcon} />
-      </Topbar>
-      }
-        <PlayerLayout
-          playerData={data}
-          progressColorFront={palette.vibrant}
-          progressColorBack={context.paletteMode === 'VIBRANT' ? palette.darkMuted : palette.darkVibrant}
-          displayMode={context.displayMode}
-        />
+      {!fullscreen.active && (
+        <Topbar>
+          <IconButton
+            onClick={() => send({ type: "SWITCH_PALETTE" })}
+            icon={Palette}
+          />
+          <IconButton
+            onClick={() => {
+              fullscreen.enter();
+              enableNoSleep();
+            }}
+            icon={FullscreenIcon}
+          />
+        </Topbar>
+      )}
+      <PlayerLayout
+        playerData={data}
+        progressColorFront={palette.vibrant}
+        progressColorBack={
+          context.paletteMode === "VIBRANT"
+            ? palette.darkMuted
+            : palette.darkVibrant
+        }
+        displayMode={context.displayMode}
+      />
     </Main>
   );
 }
